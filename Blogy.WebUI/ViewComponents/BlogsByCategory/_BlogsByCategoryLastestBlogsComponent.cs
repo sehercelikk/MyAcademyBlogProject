7b4acf33f5367766b10bd33e20d0d2f384bc6434
asus <asus@DESKTOP-1IHTQ4L>
2025-10-24 15:19:50 +0300

    .

---
+++ Blogy.WebUI/ViewComponents/BlogsByCategory/_BlogsByCategoryLastestBlogsComponent.cs
@@ -0,0 +1,14 @@
+﻿using Blogy.Business.Services.BlogServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blogy.WebUI.ViewComponents.BlogsByCategory;
+
+public class _BlogsByCategoryLastestBlogsComponent(IBlogService _blogService) : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var blogs = await _blogService.GetLast3BlogsAsync();
+        return View(blogs);
+    }
+}
