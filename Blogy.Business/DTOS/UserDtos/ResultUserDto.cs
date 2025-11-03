08d9d7257200312d038536ac1e1be2bc54a3b988
asus <asus@DESKTOP-1IHTQ4L>
2025-11-03 16:37:14 +0300

    .

--- Blogy.Business/DTOS/UserDtos/ResultUserDto.cs
+++ Blogy.Business/DTOS/UserDtos/ResultUserDto.cs
@@ -8,6 +8,5 @@ public class ResultUserDto
     public string FullName { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-    public string PhoneNumber { get; set; }
     public IList<string> Roles { get; set; }
 }
