17763b7a4aa956b72d9181b79c9b4debf61e0096
asus <asus@DESKTOP-1IHTQ4L>
2025-11-04 15:45:14 +0300

    Added ChangePassword

---
+++ Blogy.Business/DTOS/UserDtos/ChangePasswordDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blogy.Business.DTOS.UserDtos;
+
+public class ChangePasswordDto
+{
+    public string CurrentPassword { get; set; }
+    public string NewPassword { get; set; }
+
+    [Compare(nameof(NewPassword),ErrorMessage ="Şifreler birbiriyle uyumlu değil!")]
+    public string ConfirmPassword { get; set; }
+}
---
+++ Blogy.WebUI/Areas/Admin/Controllers/ChangePasswordController.cs
@@ -0,0 +1,42 @@
+﻿using Blogy.Business.DTOS.UserDtos;
+using Blogy.Entities.Concrete;
+using Blogy.WebUI.Consts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers;
+
+[Area(Roles.Admin)]
+[Authorize(Roles=Roles.Admin)]
+public class ChangePasswordController
+    (UserManager<AppUser> _userManager,SignInManager<AppUser> _signInManager) : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(ChangePasswordDto model)
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+        if(!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code,error.Description);
+            }
+            return View(model);
+        }
+        await _signInManager.SignOutAsync();
+        return Redirect("/Login/Index");
+        
+    }
+}
