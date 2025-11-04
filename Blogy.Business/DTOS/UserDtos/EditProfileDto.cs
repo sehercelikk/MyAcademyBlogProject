5003633fad81638afc8b081b3e309cd5b6713366
asus <asus@DESKTOP-1IHTQ4L>
2025-11-04 15:16:13 +0300

    Added EditProfle Post Method

--- Blogy.Business/DTOS/UserDtos/EditProfileDto.cs
+++ Blogy.Business/DTOS/UserDtos/EditProfileDto.cs
@@ -7,8 +7,8 @@ public class EditProfileDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
-    public string Title { get; set; }
-    public string ImageUrl { get; set; }
+    public string? Title { get; set; }
+    public string? ImageUrl { get; set; }
     public string CurrentPassword { get; set; }
     public IFormFile ImageFile { get; set; }
     public string Email { get; set; }
--- Blogy.WebUI/Areas/Admin/Controllers/ProfileController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/ProfileController.cs
@@ -5,6 +5,7 @@ using Blogy.WebUI.Consts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 
 namespace Blogy.WebUI.Areas.Admin.Controllers;
@@ -20,5 +21,40 @@ public class ProfileController(UserManager<AppUser> _userManager,IMapper _mapper
         return View(editUser);
     }
 
+    [HttpPost]
+    public async Task<IActionResult> Index(EditProfileDto model)
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+        var passwordCorrect = await _userManager.CheckPasswordAsync(user,model.CurrentPassword);
+        if (!passwordCorrect)
+        {
+            ModelState.AddModelError("","Şifre hatalı");
+            return View(model);
+        }
+        if(model.ImageFile != null)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory(); //Projenin olduğu Dizin
+            var extension = Path.GetExtension(model.ImageFile.FileName); // Dosyanın uzantısı
+            var imageName = Guid.NewGuid()+extension; // Resim Dosyasının ismi
+            var saveLocation = Path.Combine(currentDirectory, "wwwroot/images",imageName); // Kayıt lokasyonu
+            using var stream = new FileStream(saveLocation, FileMode.Create); // Akış oluşturuldu. işlem bitince kendi kapatsın diye using kullandık.
+            await model.ImageFile.CopyToAsync(stream); // kopyala ve akış üzerine kaydet
+            user.ImageUrl = "/images/" + imageName; // Url bilgisi atandı
+        }
+        user.FirstName = model.FirstName;
+        user.LastName = model.LastName;
+        user.Email = model.Email;
+        user.PhoneNumber = model.PhoneNumber;
+        user.UserName = model.UserName;
+        user.Title=model.Title;
+
+        var result = await _userManager.UpdateAsync(user);
+        if(!result.Succeeded)
+        {
+            ModelState.AddModelError("", "Güncelleme sırasında bir hata oluştur.");
+            return View(model);
+        }
+        return RedirectToAction("Index", "Blog", new {area=Roles.Admin});
+    }
 
 }
