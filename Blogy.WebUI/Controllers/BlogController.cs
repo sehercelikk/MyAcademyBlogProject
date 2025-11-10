77aac9e934a19a63374164e0f4384bd15014b748
asus <asus@DESKTOP-1IHTQ4L>
2025-11-10 14:51:21 +0300

    Added Pagination

--- Blogy.WebUI/Controllers/BlogController.cs
+++ Blogy.WebUI/Controllers/BlogController.cs
@@ -1,14 +1,18 @@
-﻿using Blogy.Business.Services.BlogServices;
+﻿using Blogy.Business.DTOS.BlogDtos;
+using Blogy.Business.Services.BlogServices;
 using Blogy.Business.Services.CategoryServices;
 using Microsoft.AspNetCore.Mvc;
+using PagedList.Core;
 
 namespace Blogy.WebUI.Controllers;
 
 public class BlogController(IBlogService _blogService, ICategoryService _categoryService) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int page=1, int pageSize=2)
     {
-        return View();
+        var blogs = await _blogService.GetAllAsync();
+        var values = new PagedList<ResultBlogDto>(blogs.AsQueryable(), page, pageSize);
+        return View(values);
     }
     public async Task<IActionResult> GetBlogsByCategory(int id)
     {
